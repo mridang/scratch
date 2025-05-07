d783a74c4ca5c8b170e2ebbe4d6990883b6632fd
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-05-07 20:05:57 +0300

    feat:initial commit

---
+++ release.config.js
@@ -0,0 +1,24 @@
+const packageJson = require('./package.json');
+
+module.exports = {
+	branches: ['master'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/github',
+			{
+				assets: [{ path: '*.tgz', label: 'Package' }],
+			},
+		],
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json', 'package-lock.json'],
+				message:
+					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+			},
+		],
+	],
+	repositoryUrl: packageJson.repository.url,
+};
