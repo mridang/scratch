43b44b0922e706e3bccc2f3fd9c5ab13f8a335bf
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-07-08 18:14:31 +0530

    fix: pdate release.config.js

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const packageJson = require('./package.json');
 
 module.exports = {
-	branches: ['master'],
+	branches: ['master', { name: 'beta', prerelease: 'beta' }],
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
